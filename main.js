2b5567af64355a4b9762b1e042cde6322ab65573
TingjiaInFuture <109398380+TingjiaInFuture@users.noreply.github.com>
2022-07-25 07:44:26 +0800

    update main.js

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-		$(".btn").click(function () {
-		var text = $(".search").val();
-		if ($(".btn").text() === "进入") {
-			!text.match(/^(ht|f)tp(s?):\/\//) && (text = "http://" + text);
-			history.go(-1);
-			setTimeout(function () {
-				location.href = text;
-			}, 1);
-		} else {
-			if (!text) {
-				$(".search").blur();
-			} else {
-				searchText(text);
-			}
-		}
-	});
+$(".btn").click(function () {
+  var text = $(".search").val();
+  if ($(".btn").text() === "进入") {
+    !text.match(/^(ht|f)tp(s?):\/\//) && (text = "http://" + text);
+    history.go(-1);
+    setTimeout(function () {
+      location.href = text;
+    }, 1);
+  } else {
+    if (!text) {
+      $(".search").blur();
+    } else {
+      searchText(text);
+    }
+  }
+});
 
-	$(".search").keydown(function (evt) {
-		// 使用回车键进行搜索
-		evt.keyCode === 13 && $(".btn").click();
-	});
-	// 搜索函数
-	function searchText(text) {
-		if (!text) {
-			return;
-		}
-		setTimeout(function () { // 异步执行 兼容QQ浏览器
-			
-				location.href = {
-					baidu: "https://m.baidu.com/s?wd=%s",
-					quark: "https://quark.sm.cn/s?q=%s",
-					google: "https://www.google.com/search?q=%s",
-					bing: "https://cn.bing.com/search?q=%s",
-					sm: "https://m.sm.cn/s?q=%s",
-					haosou: "https://m.so.com/s?q=%s",
-					sogou: "https://m.sogou.com/web/searchList.jsp?                                                                                keyword=%s",
-					diy: settings.get('diyEngines')
-				}[settings.get('engines')].replace("%s", text);
-			
-		}, 1);
-	}
+$(".search").keydown(function (evt) {
+  // 使用回车键进行搜索
+  evt.keyCode === 13 && $(".btn").click();
+});
+// 搜索函数
+function searchText(text) {
+  if (!text) {
+    return;
+  }
+  setTimeout(function () {
+    // 异步执行 兼容QQ浏览器
+
+    location.href = {
+      baidu: "https://m.baidu.com/s?wd=%s",
+      quark: "https://quark.sm.cn/s?q=%s",
+      google: "https://www.google.com/search?q=%s",
+      bing: "https://cn.bing.com/search?q=%s",
+      sm: "https://m.sm.cn/s?q=%s",
+      haosou: "https://m.so.com/s?q=%s",
+      sogou: "https://m.sogou.com/web/searchList.jsp?keyword=%s",
+      diy: settings.get('diyEngines')
+    }[settings.get('engines')].replace("%s", text);
+
+  }, 1);
+}
