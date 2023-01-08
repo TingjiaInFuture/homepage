ff4d28c292a4b9c15f29a4372d33aa8bed701df6
TingjiaInFuture <109398380+TingjiaInFuture@users.noreply.github.com>
2023-01-08 16:54:38 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,9 @@
-$(".btn").click(function () {
-  var text = $(".search").val();
-  if ($(".btn").text() === "进入") {
-    !text.match(/^(ht|f)tp(s?):\/\//) && (text = "http://" + text);
-    history.go(-1);
-    setTimeout(function () {
-      location.href = text;
-    }, 1);
-  } else {
-    if (!text) {
-      $(".search").blur();
-    } else {
-      searchText(text);
-    }
-  }
-});
-
-$(".search").keydown(function (evt) {
-  // 使用回车键进行搜索
-  evt.keyCode === 13 && $(".btn").click();
-});
-// 搜索函数
-function searchText(text) {
-  if (!text) {
-    return;
-  }
-  setTimeout(function () {
-    // 异步执行 兼容QQ浏览器
-    location.href = {
-      bing: "https://cn.bing.com/search?q=%s"
-    } [location.href].replace("%s", text);
-  }, 1);
+function search() {
+  var text = document.querySelector(".search").value;
+  !text.match(/^(ht|f)tp(s?):\/\//) && (text = "https://www.bing.com/search?q=" + text);
+  location.href = text;
 }
+
+document.querySelector(".search").onkeydown = function (event) {
+  event.keyCode === 13 && search();
+};
